01a694a841fd40610f9fb8a1a9d8369f4a61e761
wangyimin <wangyim6@mail2.sysu.edu.cn>
2017-04-22 18:34:45 +0800

    testing2

---
+++ src/com/testingTeam/Main.java
@@ -0,0 +1,10 @@
+package com.testingTeam;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
+
+
---
+++ src/com/testingTeam/TestingClass.java
@@ -0,0 +1,8 @@
+package com.testingTeam;
+
+/**
+ * Created by wangyimin on 2017/4/22.
+ */
+public class TestingClass {
+
+}
