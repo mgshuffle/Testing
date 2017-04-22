c36a5773fe253bb11f499ceba2d32c906353574a
wangyimin <wangyim6@mail2.sysu.edu.cn>
2017-04-22 19:41:25 +0800

    T6

---
+++ src/com/testingTeam/IJAdd.java
@@ -0,0 +1,10 @@
+package com.testingTeam;
+
+/**
+ * Created by wangyimin on 2017/4/22.
+ */
+public class IJAdd {
+	public static void sayit(){
+		System.out.println("Added");
+	}
+}
--- src/com/testingTeam/Main.java
+++ src/com/testingTeam/Main.java
@@ -1,13 +1,11 @@
 package com.testingTeam;
 
 public class Main {
-
     public static void main(String[] args) {
         // write your code here
     	EclipseAdd.saySomething();
     	System.out.println("see how it goes");
 		System.out.println("try to mess up");
+		IJAdd.sayit();
 	}
 }
-
-
