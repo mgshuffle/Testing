c454b2734f9c68d56b6645e02e334eb3a6987eb3
wangyimin <wangyim6@mail2.sysu.edu.cn>
2017-04-22 20:00:38 +0800

    T7

--- src/com/testingTeam/Main.java
+++ src/com/testingTeam/Main.java
@@ -1,5 +1,7 @@
 package com.testingTeam;
 
+import com.testingTeam.Tools.AhHa;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
@@ -7,5 +9,6 @@ public class Main {
     	System.out.println("see how it goes");
 		System.out.println("try to mess up");
 		IJAdd.sayit();
+		AhHa.say();
 	}
 }
---
+++ src/com/testingTeam/Tools/AhHa.java
@@ -0,0 +1,10 @@
+package com.testingTeam.Tools;
+
+/**
+ * Created by wangyimin on 2017/4/22.
+ */
+public class AhHa {
+	public static void say(){
+		System.out.println("Ah-Ha!");
+	}
+}
